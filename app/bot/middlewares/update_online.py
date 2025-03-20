328ba6837885c9de6e4c668ea4724238fa1c696a
Egor1-0 <johndoe@example.com>
2025-03-20 16:29:49 +0300

    add online in User model, create middlware to update online time, register middlware

---
+++ app/bot/middlewares/update_online.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from logging import getLogger
+from typing import Any, Awaitable, Callable, Dict
+
+from aiogram import BaseMiddleware
+from aiogram.types import Update, User
+from pytz import timezone
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from app.core.dao import crud_user
+
+logger = getLogger(__name__)
+
+
+class UpdateOnline(BaseMiddleware):
+    def __init__(self, session_pool: async_sessionmaker):
+        self.session_pool = session_pool
+
+    async def __call__(
+            self,
+            handler: Callable[[Update, Dict[str, Any]], Awaitable[Any]],
+            event: Update,
+            data: Dict[str, Any],
+    ) -> Any:
+        user: User = data.get("event_from_user")
+        async with self.session_pool() as db:
+            exist_user = await crud_user.get_by_id(db, id=user.id)
+            now = datetime.now(tz=timezone('Europe/Moscow'))
+            update_user_data = {"last_online": now}
+            await crud_user.update(db, db_obj=exist_user, obj_in=update_user_data)
+        return await handler(event, data)
