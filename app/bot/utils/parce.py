4aa23cb88f58c8005dd2ab7a24c5a2f942566307
daniil <daniil.dosin@onlinesup.com>
2025-03-23 18:35:08 +0300

    update main

--- app/bot/utils/parce.py
+++ app/bot/utils/parce.py
@@ -4,7 +4,9 @@ from pytz import timezone
 
 
 def parce_time(last_online: datetime):
-    if datetime.now(tz=timezone('utc')) - last_online < timedelta(minutes=15):
+    if not last_online:
+        time_text = f'🔓Был(а) давно (мск)'
+    elif datetime.now(tz=timezone('utc')) - last_online < timedelta(minutes=15):
         last_online = (last_online + timedelta(hours=3)).strftime("%H:%M")
         time_text = f'✅Был(а) в {last_online} (мск)'
     elif datetime.now(tz=timezone('utc')).date() == last_online.date():
