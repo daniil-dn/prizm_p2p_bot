f9f75b01d6a0230add8f0873f671e9b74e0b9209
Egor1-0 <johndoe@example.com>
2025-04-16 19:40:04 +0300

    fix text

--- app/utils/schedule_funcs/notification_in_channel.py
+++ app/utils/schedule_funcs/notification_in_channel.py
@@ -4,7 +4,9 @@ from pytz import timezone
 from aiogram import Bot
 
 from app.bot.utils.parce import parce_time
+from app.core.config import settings
 from app.core.dao import crud_chat_channel, crud_order_request
+from app.utils.coinmarketcap import get_currency_rate
 
 
 async def notification_sheduled(bot: Bot, session):
@@ -15,25 +17,26 @@ async def notification_sheduled(bot: Bot, session):
 
     buy_last_online_text = parce_time(best_order_to_buy.user.last_online)
     sell_last_online_text = parce_time(best_order_to_sell.user.last_online)
+    rate = await get_currency_rate("PZM", "RUB", settings.COINMARKETCAP_API_KEY)
+    now = datetime.now(tz=timezone('Europe/Moscow'))
 
     text = ('Prizm Exchange\n\n'
-            'курс руб/PZM 08:00 на 10.04.25 - 0.079\n\n'  # todo получить курс
+            f'курс руб/PZM {now.strftime("%H:%M")} на {now.strftime("%d.%m.%y")} - {rate:.4f}\n\n'  # todo получить курс
             'самый выгодный ордер на покупку:\n\n'
             f'Ордер : №{best_order_to_buy.id}\n'
-            f'Курс 1pzm - {best_order_to_buy.rate} руб\n'
+            f'Курс 1pzm - {best_order_to_buy.rate:.4f} руб\n'
             f'Лимит: {best_order_to_buy.min_limit} - {best_order_to_buy.max_limit} PZM\n'
             f'Число сделок: {best_order_to_buy.user.order_count}\n'
             f'Число отказов: {best_order_to_buy.user.cancel_order_count}\n'
             f'{buy_last_online_text}\n\n'
             f'самый выгодный ордер на продажу:\n\n'
             f'Ордер: №{best_order_to_sell.id}\n'
-            f'Курс 1pzm - {best_order_to_sell.rate} руб\n'
+            f'Курс 1pzm - {best_order_to_sell.rate:.4f} руб\n'
             f'Лимит: {best_order_to_sell.min_limit_rub} - {best_order_to_sell.max_limit_rub} руб\n'
             f'Число сделок: {best_order_to_sell.user.order_count}\n'
             f'Число отказов: {best_order_to_sell.user.cancel_order_count}\n'
             f'{sell_last_online_text}\n\n')
 
-    now = datetime.now(tz=timezone('Europe/Moscow'))
     chats = [chat for chat in chats if
              chat.current_count < chat.count_in_day and
              now >= (chat.last_post or datetime.fromtimestamp(0, timezone('Europe/Moscow'))) + timedelta(minutes=chat.interval)]
