395582c82568e802a8e048b31a7f50c20948986b
daniil <daniil.dosin@onlinesup.com>
2025-04-22 12:16:15 +0300

    fixes

--- app/bot/ui/texts.py
+++ app/bot/ui/texts.py
@@ -1,5 +1,5 @@
 def get_start_text(balance, referral_balance, order_count, cancel_order_count):
-    return f"""Ваш баланс: {balance} PZM
-Ваш реферальный баланс: {referral_balance}
+    return f"""Ваш баланс: {balance:.3f} PZM
+Ваш реферальный баланс: {referral_balance:.3f}
 Кол-во сделок: {order_count}
 Кол-во отказов: {cancel_order_count}"""
